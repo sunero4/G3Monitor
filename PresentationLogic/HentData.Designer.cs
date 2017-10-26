d1a1b94c69ab0b64f92531fdc35b347d20372090
sunero4 <anderskloborg@gmail.com>
2017-10-26 11:48:34 +0200

    Startet på Hent data Form

---
+++ PresentationLogic/HentData.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace PresentationLogic
+{
+    partial class HentData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HentData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 486);
+            this.Name = "HentData";
+            this.Text = "G3-monitor: Måling";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
