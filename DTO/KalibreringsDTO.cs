b77763c923601080637c6667a65d3ea08fb5ba10
sunero4 <anderskloborg@gmail.com>
2017-11-09 11:47:53 +0100

    Started messing around with writing and reading to xml files, but tested most of it in a separate project to see the output, so no real results here

---
+++ DTO/KalibreringsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KalibreringsDTO
+    {
+        public List<int> ExpectedValue { get; set; }
+        public List<int> ActualValue { get; set; }
+    }
+}
---
+++ DataAccessLogic/CalibrationRetrieval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DTO;
+
+namespace DataAccessLogic
+{
+    class CalibrationRetrieval : ICalibrationDataRetrieval
+    {
+        public KalibreringsDTO GetCalibrationData()
+        {
+            var calibrations = XDocument.Load(FileInformation.FilePath);
+
+            
+        }
+    }
+}
