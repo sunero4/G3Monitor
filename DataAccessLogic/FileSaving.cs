b1e18ac6d36c0423a0ea1dafd57304d13ab292b6
sunero4 <anderskloborg@gmail.com>
2017-11-13 15:26:37 +0100

    Started on filesaving for blood pressure

--- DataAccessLogic/FileSaving.cs
+++ DataAccessLogic/FileSaving.cs
@@ -6,30 +6,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
-using System.Xml.Serialization;
 using DTO;
 
 namespace DataAccessLogic
 {
     class FileSaving : ISaving
     {
-        public void SaveBloodPressureData(PatientDTO patient)
+        private XDocument _xDoc;
+        public void SaveNewPatient(MaalingDTO maaling, string cpr)
         {
-            try
-            {
-                var xSerializer = new XmlSerializer(typeof(PatientDTO));
+            _xDoc = XDocument.Load(FileInformation.FilePath);
 
-                using (var writer = new StreamWriter(FileInformation.FilePath))
-                {
-                    xSerializer.Serialize(writer, patient);
-                }
-            }
-            catch (XmlException e)
-            {
-                Console.WriteLine(e.Message);
-                //Handle exception here
-            }
+            var e = _xDoc.Root;
 
+            e.Add(new XElement("Maaling"),
+                new XAttribute("ID", maaling.MaaleID),
+                new XAttribute("MaaleTidspunkt", maaling.MaaleTidspunkt),
+                new XAttribute("CPR", cpr)
+                );
+            e.Add(new XElement("Kommentar", maaling.Kommentar));
+
+            _xDoc.Save(FileInformation.FilePath);
         }
+
+        public void SaveValues(MaalingDTO maaling, string cpr)
+        {
+            _xDoc = XDocument.Load(FileInformation.FilePath);
+
+            var e = _xDoc.Descendants("Maaling")
+                .FirstOrDefault(x => (string) x.Attribute("CPR") == cpr);
+
+            if (e != null)
+            {
+                e.Add(new XElement("Maalingdata", Convert.ToBase64String(maaling.MaaleData)));
+            }
+            _xDoc.Save(FileInformation.FilePath);
+        }
+
     }
 }
