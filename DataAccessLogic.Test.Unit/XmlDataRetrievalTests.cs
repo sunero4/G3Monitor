90487a735a79ffe94568d2c225266f40699156ee
sunero4 <anderskloborg@gmail.com>
2017-11-16 14:42:11 +0100

    Transer

--- DataAccessLogic.Test.Unit/XmlDataRetrievalTests.cs
+++ DataAccessLogic.Test.Unit/XmlDataRetrievalTests.cs
@@ -12,13 +12,26 @@ namespace DataAccessLogic.Test.Unit
     class XmlDataRetrievalTests
     {
         private MeasurementRetrievalXml _measurementRetrieval;
+        private string _baseSingleInput;
+        private string _baseCombinedInput;
+        private byte[] _input1;
+        private byte[] _input2;
         [SetUp]
         public void Setup()
         {
+            _input1 = new byte[5] {42, 75, 23, 4, 123};
+            _input2 = new byte[5] {32, 67, 98, 124, 9};
+            _baseSingleInput = Convert.ToBase64String(_input1);
+            _baseCombinedInput = String.Concat(Convert.ToBase64String(_input1), Convert.ToBase64String(_input2));
             _measurementRetrieval = new MeasurementRetrievalXml();
         }
 
-        public void 
+        [Test]
+        public void MeasurementRetrieval_ConvertFromBase64OneInput_OutputMatchesExpected()
+        {
+            var output = Convert.FromBase64String(_baseSingleInput);
+            Assert.That(output.SequenceEqual(_measurementRetrieval.ConvertToByteArray(_baseSingleInput)));
+        }
 
     }
 }
--- DataAccessLogic/FileToDatabaseTransfer.cs
+++ DataAccessLogic/FileToDatabaseTransfer.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO;
 
 namespace DataAccessLogic
 {
     public class FileToDatabaseTransfer
     {
+        private IRetrievedData<MaalingDTO> _dataRetrieval;
+        private ISaving _saving;
+        private PatientDTO _patient;
 
+        public FileToDatabaseTransfer(PatientDTO patient)
+        {
+            _patient = patient;
+            _dataRetrieval = new MeasurementRetrievalXml();
+            _saving = new DatabaseSaving();
+        }
 
+        public void TransferToDatabase()
+        {
+            GetData();
+            UploadToDatabase();
+        }
 
+        private void GetData()
+        {
+            _dataRetrieval.HentData(_patient);
+        }
+
+        private void UploadToDatabase()
+        {
+            _saving.SaveBloodPressureData(_patient);
+        }
     }
 }
