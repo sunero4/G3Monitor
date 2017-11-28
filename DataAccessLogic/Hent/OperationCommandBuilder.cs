e9f94ea188ac677b90ddf0af34ceb6bd7980c772
sunero4 <anderskloborg@gmail.com>
2017-11-28 17:02:22 +0100

    Merge

---
+++ DataAccessLogic/Hent/OperationCommandBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DataAccessLogic.Hent
+{
+    public class OperationCommandBuilder : ICommandBuilder<PatientDTO>
+    {
+        public SqlCommand BuildCommand(PatientDTO input, SqlConnection conn, string query)
+        {
+            var cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@cpr", input.CPR);
+
+            return cmd;
+        }
+    }
+}
---
+++ DataAccessLogic/Hent/OperationQueryBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DataAccessLogic.Hent
+{
+    public class OperationQueryBuilder : IQueryBuilder<List<OperationsDTO>>
+    {
+        private StringBuilder _stringBuilder;
+
+        public OperationQueryBuilder()
+        {
+            _stringBuilder = new StringBuilder();
+        }
+
+        public string BuildQuery(List<OperationsDTO> input)
+        {
+            _stringBuilder.Append("SELECT * FROM OperationsData WHERE CPR = @cpr OUTPUT INSERTED OperationsID");
+            return _stringBuilder.ToString();
+        }
+    }
+}
